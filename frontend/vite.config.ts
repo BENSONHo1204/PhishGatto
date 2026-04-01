8609212f1713f2fc4d16db2502d878c5e3354029
Ho Benson <hobenson2004@gmail.com>
2026-04-01 16:04:50 +0800

    fix: vite base path + remove proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/PhishGatto/",
+  base: "./",
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:5000",
-        changeOrigin: true
-      }
-    }
-  }
 });
